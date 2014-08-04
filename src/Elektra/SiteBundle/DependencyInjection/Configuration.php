896b2ecb0bb1e04f9f70cd203da24a30b4770919
florian@eichler.co.at <9gamunde#9>
2014-08-04 13:14:57 +0200

    Theme basics Basic config for seed unit models & power types

---
+++ src/Elektra/SiteBundle/DependencyInjection/Configuration.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Elektra\SiteBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+
+    /**
+     * Generates the configuration tree builder.
+     *
+     * @return \Symfony\Component\Config\Definition\Builder\TreeBuilder The tree builder
+     */
+    public function getConfigTreeBuilder()
+    {
+
+        $treeBuilder = new TreeBuilder();
+        $rootNode    = $treeBuilder->root('elektra_site');
+
+        return $treeBuilder;
+    }
+
+
+}
