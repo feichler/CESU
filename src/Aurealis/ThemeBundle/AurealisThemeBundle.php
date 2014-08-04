17eba55e33e8a013260445ba25f556a6e2c8dbcb
florian@eichler.co.at <9gamunde#9>
2014-08-04 13:07:31 +0200

    Theme basics Basic config for seed unit models & power types

--- src/Aurealis/ThemeBundle/AurealisThemeBundle.php
+++ src/Aurealis/ThemeBundle/AurealisThemeBundle.php
@@ -2,9 +2,19 @@
 
 namespace Aurealis\ThemeBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class AurealisThemeBundle extends Bundle
 {
+
+    public function build(ContainerBuilder $container)
+    {
+
+        parent::build($container);
+
+
+    }
+
 
 }
