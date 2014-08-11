584b41d6e1c74284a75799b30b8669ace80e2913
Alexander Spengler <skudder1982@gmail.com>
2014-08-11 10:58:38 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	src/Elektra/SeedBundle/Entity/SeedUnits/SeedUnitModel.php

--- src/Elektra/SeedBundle/Repositories/Companies/PersonRepository.php
+++ src/Elektra/SeedBundle/Repositories/Companies/PersonRepository.php
@@ -6,8 +6,10 @@ use Doctrine\ORM\EntityRepository;
 
 class PersonRepository extends EntityRepository
 {
+
     public function getCount()
     {
+
         $builder = $this->getEntityManager()->createQueryBuilder();
         $builder->select($builder->expr()->count('p'));
         $builder->from($this->getEntityName(), 'p');
@@ -15,5 +17,13 @@ class PersonRepository extends EntityRepository
         $query = $builder->getQuery();
 
         return $query->getSingleScalarResult();
+    }
+
+    public function getEntries($page, $perPage)
+    {
+
+        $entries = $this->findBy(array(), array(), $perPage, ($page - 1) * $perPage);
+
+        return $entries;
     }
 }
