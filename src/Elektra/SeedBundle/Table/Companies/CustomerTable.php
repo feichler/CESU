d3d0bac10ed5f00e3cdd839f99d1aff4e8a644ef
Alexander Spengler <skudder1982@gmail.com>
2014-08-26 13:05:03 +0200

    added tables for Customer, SalesTeam

--- src/Elektra/SeedBundle/Table/Companies/CustomerTable.php
+++ src/Elektra/SeedBundle/Table/Companies/CustomerTable.php
@@ -1,7 +1,22 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Florian
- * Date: 25.08.14
- * Time: 16:08
- */ 
+
+namespace Elektra\SeedBundle\Table\Companies;
+
+use Elektra\CrudBundle\Table\Table;
+
+class CustomerTable extends Table
+{
+
+    protected function initialiseColumns()
+    {
+        $shortName = $this->getColumns()->addTitleColumn('table.companies.company.shortName');
+        $shortName->setFieldData('shortName');
+        $shortName->setSearchable();
+        $shortName->setSortable();
+
+        $name = $this->getColumns()->add('table.companies.company.name');
+        $name->setFieldData('name');
+        $name->setSearchable();
+        $name->setSortable();
+    }
+}
