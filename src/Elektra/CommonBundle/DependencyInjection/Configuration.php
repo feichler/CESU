a38b4e8c023ee99b088dd5741cf1222e38f18de2
florian@eichler.co.at <9gamunde#9>
2014-08-26 09:45:54 +0200

    Fixed form-action / crud-action error

---
+++ src/Elektra/CommonBundle/DependencyInjection/Configuration.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @author    Florian Eichler <florian@eichler.co.at>
+ * @author    Alexander Spengler <alexander.spengler@habanero-it.eu>
+ * @copyright 2014 Florian Eichler, Alexander Spengler. All rights reserved.
+ * @license   MINOR add a license
+ * @version   0.1-dev
+ */
+
+namespace Elektra\CommonBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * Class Configuration
+ *
+ * @package Elektra\CommonBundle\DependencyInjection
+ *
+ * @version 0.1-dev
+ */
+class Configuration implements ConfigurationInterface
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+
+        $treeBuilder = new TreeBuilder();
+
+        $rootNode = $treeBuilder->root('elektra_common');
+
+        // NOTE: configuration options for the common bundle needed?
+
+        return $treeBuilder;
+    }
+}
---
+++ src/Elektra/CommonBundle/ElektraCommonBundle.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @author    Florian Eichler <florian@eichler.co.at>
+ * @author    Alexander Spengler <alexander.spengler@habanero-it.eu>
+ * @copyright 2014 Florian Eichler, Alexander Spengler. All rights reserved.
+ * @license   MINOR add a license
+ * @version   0.1-dev
+ */
+
+namespace Elektra\CommonBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class ElektraCommonBundle
+ *
+ * @package Elektra\CommonBundle
+ *
+ * @version 0.1-dev
+ */
+class ElektraCommonBundle extends Bundle
+{
+
+}
