871ac4e18d536f29e7b90753fa09b92eb0e6eb9f
Alexander Spengler <skudder1982@gmail.com>
2014-08-26 14:57:05 +0200

    added table for ContactInfoType

--- src/Elektra/SeedBundle/Table/Companies/ContactInfoTable.php
+++ src/Elektra/SeedBundle/Table/Companies/ContactInfoTable.php
@@ -9,6 +9,20 @@ class ContactInfoTable extends Table
 
     protected function initialiseColumns()
     {
+        $text = $this->getColumns()->addTitleColumn('table.companies.contactInfo.text');
+        $text->setFieldData("text");
+        $text->setSortable();
+        $text->setSearchable();
 
+        $name = $this->getColumns()->add('table.companies.contactInfo.name');
+        $name->setFieldData('name');
+        $name->setSortable();
+        $name->setSearchable();
+
+        $contactInfoType = $this->getColumns()->add('table.companies.contactInfo.contactInfoType');
+        $contactInfoType->setDefinition($this->getCrud()->getDefinition('Elektra', 'Seed', 'Companies', 'ContactInfoType'));
+        $contactInfoType->setFieldData('contactInfoType.name');
+        $contactInfoType->setSortable();
+        $contactInfoType->setFilterable()->setFieldFilter('name');
     }
 }
