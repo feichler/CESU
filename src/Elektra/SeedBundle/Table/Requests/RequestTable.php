fc72556a87eba6fa95bff9c19ffe695a1362b518
florian@eichler.co.at <9gamunde#9>
2014-08-29 13:44:43 +0200

    Undoing some overwrites

--- src/Elektra/SeedBundle/Table/Requests/RequestTable.php
+++ src/Elektra/SeedBundle/Table/Requests/RequestTable.php
@@ -26,20 +26,19 @@ class RequestTable extends Table
 
         $requester = $this->getColumns()->add('table.requests.request.requester');
         $requester->setDefinition($this->getCrud()->getDefinition('Elektra', 'Seed', 'Companies', 'CompanyPerson'));
-        $requester->setFieldData('requester.title');
+        $requester->setFieldData('requesterPerson.title');
         $requester->setSortable();
-//        $requester->setFilterable()->setFieldFilter('name');
+        //        $requester->setFilterable()->setFieldFilter('name');
 
         $receiver = $this->getColumns()->add('table.requests.request.receiver');
         $receiver->setDefinition($this->getCrud()->getDefinition('Elektra', 'Seed', 'Companies', 'CompanyPerson'));
-        $receiver->setFieldData('receiver.title');
+        $receiver->setFieldData('receiverPerson.title');
         $receiver->setSortable();
-//        $receiver->setFilterable()->setFieldFilter('name');
+        //        $receiver->setFilterable()->setFieldFilter('name');
 
         $shippingAddress = $this->getColumns()->add('table.requests.request.shippingLocation');
         $shippingAddress->setDefinition($this->getCrud()->getDefinition('Elektra', 'Seed', 'Companies', 'CompanyLocation'));
         $company->setFieldData(array('shippingLocation.shortName', 'shippingLocation.name'));
         $shippingAddress->setSortable();
-
     }
 }
