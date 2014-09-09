80377486800865df2577c6edcd245e91d3536de0
florian@eichler.co.at <9gamunde#9>
2014-09-09 14:11:10 +0200

    Add Seed Units to request rendering - Part 1

---
+++ src/Elektra/SiteBundle/Form/Field/DisplayType.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Elektra\SiteBundle\Form\Field;
+
+use Symfony\Component\Form\AbstractType;
+
+class DisplayType extends AbstractType
+{
+
+    /**
+     * Returns the name of this type.
+     *
+     * @return string The name of this type
+     */
+    public function getName()
+    {
+
+        return 'display';
+    }
+
+
+}
