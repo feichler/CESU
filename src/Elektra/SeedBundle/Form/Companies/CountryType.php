4b01ff67b7ce997cd121a2bf278a7477c645bb67
Alexander Spengler <skudder1982@gmail.com>
2014-09-26 16:50:41 +0200

    fix #51, fix #50

--- src/Elektra/SeedBundle/Form/Companies/CountryType.php
+++ src/Elektra/SeedBundle/Form/Companies/CountryType.php
@@ -28,14 +28,31 @@ class CountryType extends CrudForm
     {
         $common = $this->addFieldGroup($builder, $options, 'common');
 
-        $regionFieldOptions = $this->getFieldOptions('region');
-        $regionFieldOptions->required()->notBlank();
-        $regionFieldOptions->add('class'    , $this->getCrud()->getDefinition('Elektra', 'Seed', 'Companies', 'Region')->getClassEntity());
-        $regionFieldOptions->add(    'property' , 'name');
-        $common->add('region','entity',$regionFieldOptions->toArray());
-        $common->add('name', 'text', $this->getFieldOptions('name')->required()->notBlank()->toArray());
-        $common->add('alphaTwo', 'text', $this->getFieldOptions('alphaTwo')->required()->notBlank()->toArray());
-        $common->add('alphaThree', 'text', $this->getFieldOptions('alphaThree')->required()->notBlank()->toArray());
-        $common->add('numericCode', 'text', $this->getFieldOptions('numericCode')->required()->notBlank()->toArray());
+        $common->add('name', 'text', $this->getFieldOptions('name')
+            ->required()
+            ->notBlank()
+            ->toArray());
+
+        $common->add('region','entity', $this->getFieldOptions('region')
+            ->required()
+            ->notBlank()
+            ->add('class', $this->getCrud()->getDefinition('Elektra', 'Seed', 'Companies', 'Region')->getClassEntity())
+            ->add('property' , 'name')
+            ->toArray());
+
+        $common->add('alphaTwo', 'text', $this->getFieldOptions('alphaTwo')
+            ->required()
+            ->notBlank()
+            ->toArray());
+
+        $common->add('alphaThree', 'text', $this->getFieldOptions('alphaThree')
+            ->required()
+            ->notBlank()
+            ->toArray());
+
+        $common->add('numericCode', 'text', $this->getFieldOptions('numericCode')
+            ->required()
+            ->notBlank()
+            ->toArray());
     }
 }
